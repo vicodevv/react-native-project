543d88c37c7a496e141b6aa9aef97e05ceb37f1b
Victor Omoniyi <vomoniyi2001@gmail.com>
2023-03-03 11:34:18 +0100

    Sign Up page

--- App.js
+++ App.js
@@ -1,11 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
 
-export default function App() {
+export default function Signup() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const handleSignup = () => {
+    // handle sign-up logic here
+    console.log('Signing up...');
+  }
+
   return (
     <View style={styles.container}>
-      <Text>HELLO</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Hello there, welcome to Charge</Text>
+      <Text style={styles.subtitle}>Kindly key in your details to create an account</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Username"
+        value={username}
+        onChangeText={setUsername}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+      />
+      <Button title="Sign Up" onPress={handleSignup} />
     </View>
   );
 }
@@ -13,8 +36,27 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+    backgroundColor: '#0E0E0E',
   },
-}); 
+  title: {
+    fontSize: 24,
+    marginBottom: 20,
+    color: '#fff',
+  },
+  subtitle: {
+    fontSize: 16,
+    marginBottom: 20,
+    color: '#fff',
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 10,
+    width: '100%',
+  },
+});
